5c58ef8c9a2abc86e03bb5a9aa40f3c7fb955d56
Borja Abad <borjaabad@gmail.com>
2013-11-18 00:47:19 +0100

    Módulos dinámicos en el front La página se presenta como una agregación de módulos, y pueden ser seteados en la maquetación de la página haciendo drag&drop

---
+++ www/back/components/paginas/models/paginaModel_1.php
@@ -0,0 +1,92 @@
+<?php
+    
+class paginaModel extends Model{
+    
+        public $id;
+	public $titulo;
+	public $plantilla;
+            
+            
+	public function __construct(){
+		parent::__construct();
+                $this->cargaBD();
+	}
+
+        public function getPaginas() {
+            
+		try {	
+                        $paginas = new paginaModel();
+			$query = "SELECT * FROM paginas";
+                        $paginas = $paginas->_db->query($query) or die(mysql_error().mysql_errno());
+                        $paginas = $paginas->fetchAll(PDO::FETCH_ASSOC);
+                            
+                        return $paginas;
+		}
+		catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		}
+	}
+        
+        public function getPagina($id){
+            
+                    
+           	try {	
+                        $paginas = new paginaModel();
+			$query = "SELECT enlace FROM paginas WHERE id = '" . $this->validaInt($id). "'";
+                        $paginas = $paginas->_db->query($query) or die(mysql_error().mysql_errno());
+                               
+                        
+
+                        $pagina = $paginas->fetch();
+
+                        return $pagina;
+		}
+		catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		}
+        }
+
+
+        public function savePagina(){
+            
+		try {
+			$conn = $this->_db->prepare("INSERT INTO paginas (titulo, descripcion, enlace) VALUES (?, ?, ?)") or die(mysql_error().mysql_errno());
+                        if($conn->execute(array($this->getTexto('titulo'),$this->getTexto('descripcion'),  Controller::normalizaStringToUrlFilesystemMatch($this->getTexto('titulo'))))){
+                                    return true;
+                        }
+                            
+                        return false;
+		}
+		catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		}
+        }
+        
+        public function eliminar($paginas){
+                $proccess = true;
+		
+                try {
+			$conn = $this->_db->prepare("DELETE FROM paginas WHERE id = ?");
+			foreach ($paginas as $id){
+				if(!$conn->execute(array($id)))
+					$proccess = false;
+			}
+		
+                if(!$proccess)
+                    return false;
+                return true;
+                }
+		catch (PDOException $e) {
+			echo $e->getMessage();
+			return false;
+		}
+	}
+        
+    
+        
+}
+    
+?>
---
+++ www/modules/secciones/controllers/indexController.php
@@ -0,0 +1,53 @@
+<?php
+
+class indexController extends Controller{
+
+
+	public function __construct(){
+		parent::__construct();
+		Session::control();
+	}
+
+	//secciones/
+	public function index(){
+		$seccion = $this->loadModel('seccion');
+		$this->_view->_secciones = $seccion->getSecciones();
+		$this->_view->renderizar('listado',true);
+	}
+
+	//secciones/agregar
+	public function agregar(){
+		if($_POST){
+			$seccion = $this->loadModel('seccion');
+			$postSeccion = $this->getTexto('seccion');
+			if($postSeccion == ""){
+				Alertify::add('La sección no puede ser vacía','error');
+			}
+			else{
+				if($result = $seccion->addSeccion($postSeccion))
+					Alertify::add('Se agregó correctamente la sección','success');
+				else if(!$result)
+					Alertify::add('Hubo un error. La sección no fue agregada','error');
+			}
+		}
+		header('location: '.BASE_URL_COM);
+	}
+
+	//secciones/eliminar
+	public function eliminar(){
+		if(isset($_POST['secciones'])) {
+			$secciones = $_POST['secciones'];
+			$seccion = $this->loadModel('seccion');
+			
+			if($seccion->eliminar($secciones))
+				Alertify::add('Eliminación OK','success');
+			else 
+				Alertify::add('Hubo un problema con la eliminación.','error');
+		}else{
+			Alertify::add('No seleccionó ninguna sección.','log');
+		}
+		header('location: '.BASE_URL_COM);
+	}
+}
+
+?>
